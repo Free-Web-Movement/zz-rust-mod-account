516a25b635b5e87f03d2444fb4b8693dad640cc5
calidion <calidion@gmail.com>
2025-12-17 01:20:06 +0800

    added to public_key and to private key

--- src/address.rs
+++ src/address.rs
@@ -1,23 +1,19 @@
-use bip39::{ Language, Mnemonic };
+use bip39::{Language, Mnemonic};
 use bitcoin::address::AddressType;
-use bitcoin::bip32::{ DerivationPath, Xpriv };
-use bitcoin::{ Address, Network, PrivateKey, PublicKey };
+use bitcoin::bip32::{DerivationPath, Xpriv};
+use bitcoin::{Address, Network, PrivateKey, PublicKey};
 use hmac::Hmac;
 use pbkdf2::pbkdf2;
-use secp256k1::{ Message, Secp256k1, ecdsa::Signature };
-use sha2::{ Digest, Sha256, Sha512 };
-use std::{ fmt, fs };
-use std::io::{ self, Write, Read };
+use secp256k1::{Message, Secp256k1, ecdsa::Signature};
+use sha2::{Digest, Sha256, Sha512};
+use std::io::{self, Read, Write};
 use std::str::FromStr;
+use std::{fmt, fs};
 
-use serde::{ Serialize, Deserialize };
+use serde::{Deserialize, Serialize};
 
 use crate::basic::{
-    COIN_PREFIX,
-    DERIVATION_PATH,
-    MNEMONIC_SEED_ROUNDS,
-    MNEMONIC_SEED_SIZE,
-    MNEMONIC_STR,
+    COIN_PREFIX, DERIVATION_PATH, MNEMONIC_SEED_ROUNDS, MNEMONIC_SEED_SIZE, MNEMONIC_STR,
     MNEMONIC_WORD_COUNT,
 };
 
@@ -56,14 +52,18 @@ pub struct FreeWebMovementAddress {
 
 pub mod serde_prefix {
     use super::*;
-    use serde::{ Serializer, Deserializer };
+    use serde::{Deserializer, Serializer};
 
-    pub fn serialize<S>(prefix: &str, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
+    pub fn serialize<S>(prefix: &str, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
         serializer.serialize_str(prefix)
     }
 
     pub fn deserialize<'de, D>(deserializer: D) -> Result<String, D::Error>
-        where D: Deserializer<'de>
+    where
+        D: Deserializer<'de>,
     {
         let s = String::deserialize(deserializer)?;
         Ok(s)
@@ -71,16 +71,18 @@ pub mod serde_prefix {
 }
 pub mod serde_mnemonic {
     use super::*;
-    use serde::{ Serializer, Deserializer };
+    use serde::{Deserializer, Serializer};
 
     pub fn serialize<S>(mnemonic: &Mnemonic, serializer: S) -> Result<S::Ok, S::Error>
-        where S: Serializer
+    where
+        S: Serializer,
     {
         serializer.serialize_str(&mnemonic.to_string())
     }
 
     pub fn deserialize<'de, D>(deserializer: D) -> Result<Mnemonic, D::Error>
-        where D: Deserializer<'de>
+    where
+        D: Deserializer<'de>,
     {
         let s = String::deserialize(deserializer)?;
         Mnemonic::parse(&s).map_err(serde::de::Error::custom)
@@ -89,35 +91,41 @@ pub mod serde_mnemonic {
 
 pub mod serde_address {
     use super::*;
-    use serde::{ Serializer, Deserializer };
+    use serde::{Deserializer, Serializer};
 
     pub fn serialize<S>(address: &Address, serializer: S) -> Result<S::Ok, S::Error>
-        where S: Serializer
+    where
+        S: Serializer,
     {
         serializer.serialize_str(&address.to_string())
     }
 
     pub fn deserialize<'de, D>(deserializer: D) -> Result<Address, D::Error>
-        where D: Deserializer<'de>
+    where
+        D: Deserializer<'de>,
     {
         let s = String::deserialize(deserializer)?;
         let unchecked = Address::from_str(&s).map_err(serde::de::Error::custom)?;
-        unchecked.require_network(Network::Bitcoin).map_err(serde::de::Error::custom)
+        unchecked
+            .require_network(Network::Bitcoin)
+            .map_err(serde::de::Error::custom)
     }
 }
 
 pub mod serde_pubkey {
     use super::*;
-    use serde::{ Serializer, Deserializer };
+    use serde::{Deserializer, Serializer};
 
     pub fn serialize<S>(pk: &PublicKey, serializer: S) -> Result<S::Ok, S::Error>
-        where S: Serializer
+    where
+        S: Serializer,
     {
         serializer.serialize_str(&pk.to_string())
     }
 
     pub fn deserialize<'de, D>(deserializer: D) -> Result<PublicKey, D::Error>
-        where D: Deserializer<'de>
+    where
+        D: Deserializer<'de>,
     {
         let s = String::deserialize(deserializer)?;
         PublicKey::from_str(&s).map_err(serde::de::Error::custom)
@@ -126,16 +134,18 @@ pub mod serde_pubkey {
 
 pub mod serde_privkey {
     use super::*;
-    use serde::{ Serializer, Deserializer };
+    use serde::{Deserializer, Serializer};
 
     pub fn serialize<S>(sk: &PrivateKey, serializer: S) -> Result<S::Ok, S::Error>
-        where S: Serializer
+    where
+        S: Serializer,
     {
         serializer.serialize_str(&sk.to_wif())
     }
 
     pub fn deserialize<'de, D>(deserializer: D) -> Result<PrivateKey, D::Error>
-        where D: Deserializer<'de>
+    where
+        D: Deserializer<'de>,
     {
         let s = String::deserialize(deserializer)?;
         PrivateKey::from_wif(&s).map_err(serde::de::Error::custom)
@@ -147,15 +157,11 @@ impl FreeWebMovementAddress {
     pub fn new(mnemonic_info: MnemonicInfo, address_info_option: Option<AddressInfo>) -> Self {
         let mnemonic: Mnemonic;
         if mnemonic_info.phrase.is_empty() {
-            mnemonic = Mnemonic::generate_in(
-                mnemonic_info.language,
-                mnemonic_info.word_count
-            ).unwrap();
+            mnemonic =
+                Mnemonic::generate_in(mnemonic_info.language, mnemonic_info.word_count).unwrap();
         } else {
-            mnemonic = Mnemonic::parse_in(
-                mnemonic_info.language,
-                mnemonic_info.phrase.clone()
-            ).unwrap();
+            mnemonic =
+                Mnemonic::parse_in(mnemonic_info.language, mnemonic_info.phrase.clone()).unwrap();
         }
 
         // 默认地址信息
@@ -169,18 +175,20 @@ impl FreeWebMovementAddress {
 
         let seed: [u8; MNEMONIC_SEED_SIZE] = FreeWebMovementAddress::mnemonic_to_seed(
             &mnemonic.clone(),
-            &mnemonic_info.passphrase.clone()
+            &mnemonic_info.passphrase.clone(),
         );
         let (public_key, private_key) = FreeWebMovementAddress::to_key_pair(
             seed,
             &address_info.derivation_path,
-            address_info.network
-        ).unwrap();
+            address_info.network,
+        )
+        .unwrap();
         let address = FreeWebMovementAddress::key_to_inner_address(
             public_key,
             address_info.network,
-            address_info.address_type
-        ).unwrap();
+            address_info.address_type,
+        )
+        .unwrap();
 
         FreeWebMovementAddress {
             prefix: address_info.prefix,
@@ -200,7 +208,7 @@ impl FreeWebMovementAddress {
             mnemonic.to_string().as_bytes(),
             salt.as_bytes(),
             MNEMONIC_SEED_ROUNDS,
-            &mut seed
+            &mut seed,
         );
         seed
     }
@@ -208,7 +216,7 @@ impl FreeWebMovementAddress {
     pub fn key_to_inner_address(
         key: PublicKey,
         network: Network,
-        address_type: AddressType // 新增参数
+        address_type: AddressType, // 新增参数
     ) -> Result<Address, String> {
         let address = match address_type {
             AddressType::P2pkh => Address::p2pkh(&key, network),
@@ -224,7 +232,7 @@ impl FreeWebMovementAddress {
     pub fn to_key_pair(
         seed: [u8; MNEMONIC_SEED_SIZE],
         dp: &str,
-        network: Network
+        network: Network,
     ) -> Result<(PublicKey, PrivateKey), String> {
         let secp = Secp256k1::new();
         let xprv = Xpriv::new_master(network, &seed).map_err(|e| e.to_string())?;
@@ -246,7 +254,16 @@ impl FreeWebMovementAddress {
         let secp = Secp256k1::new();
         let hash = Sha256::digest(msg);
         let message = Message::from_digest_slice(&hash).unwrap();
-        secp.verify_ecdsa(&message, signature, &public_key.inner).is_ok()
+        secp.verify_ecdsa(&message, signature, &public_key.inner)
+            .is_ok()
+    }
+
+    pub fn to_public_key(bytes: &Vec<u8>) -> PublicKey {
+        PublicKey::from_slice(&bytes).unwrap()
+    }
+
+    pub fn to_private_key(bytes: &Vec<u8>) -> PrivateKey {
+        PrivateKey::from_slice(&bytes, Network::Bitcoin).unwrap()
     }
 
     pub fn random() -> Self {
@@ -261,8 +278,7 @@ impl FreeWebMovementAddress {
     }
 
     pub fn save_to_file(&self, path: &str) -> io::Result<()> {
-        let json = serde_json
-            ::to_string_pretty(self)
+        let json = serde_json::to_string_pretty(self)
             .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
         let mut file = fs::File::create(path)?;
         file.write_all(json.as_bytes())?;
@@ -270,7 +286,8 @@ impl FreeWebMovementAddress {
     }
 
     pub fn from_json(json: &str) -> io::Result<Self> {
-        let addr = serde_json::from_str(json).map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+        let addr =
+            serde_json::from_str(json).map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
         Ok(addr)
     }
 
@@ -297,11 +314,11 @@ mod tests {
     // use crate::address::COIN_PREFIX;
     // use crate::address::DERIVATION_PATH;
     // use crate::address::MNEMONIC_WORD_COUNT;
+    use crate::basic::*;
     use bip39::Language;
     use bip39::Mnemonic;
     use bitcoin::AddressType;
     use bitcoin::Network;
-    use crate::basic::*;
     #[test]
     fn it_should_generate() {
         let mi_en: MnemonicInfo = MnemonicInfo {
@@ -318,15 +335,21 @@ mod tests {
 
         println!(
             "生成助记词: {}",
-            Mnemonic::generate_in(mi_en.language, mi_en.word_count).unwrap().to_string()
+            Mnemonic::generate_in(mi_en.language, mi_en.word_count)
+                .unwrap()
+                .to_string()
         );
         println!(
             "生成助记词: {}",
-            Mnemonic::generate_in(mi_scn.language, mi_en.word_count).unwrap().to_string()
+            Mnemonic::generate_in(mi_scn.language, mi_en.word_count)
+                .unwrap()
+                .to_string()
         );
         println!(
             "生成助记词: {}",
-            Mnemonic::generate_in(mi_tcn.language, mi_en.word_count).unwrap().to_string()
+            Mnemonic::generate_in(mi_tcn.language, mi_en.word_count)
+                .unwrap()
+                .to_string()
         );
 
         let ai = AddressInfo {
@@ -349,7 +372,7 @@ mod tests {
             language: Language::English,
             word_count: MNEMONIC_WORD_COUNT,
             phrase: String::from(
-                "legal winner thank year wave sausage worth useful legal winner thank yellow"
+                "legal winner thank year wave sausage worth useful legal winner thank yellow",
             ),
             passphrase: String::new(),
         };
@@ -358,7 +381,7 @@ mod tests {
             language: Language::English,
             word_count: MNEMONIC_WORD_COUNT,
             phrase: String::from(
-                "legal winner thank year wave sausage worth useful legal winner thank yellow"
+                "legal winner thank year wave sausage worth useful legal winner thank yellow",
             ),
             passphrase: String::new(),
         };
@@ -367,7 +390,7 @@ mod tests {
             language: Language::English,
             word_count: MNEMONIC_WORD_COUNT,
             phrase: String::from(
-                "public refuse price sadness winter nose finger bomb damage corn expect marble"
+                "public refuse price sadness winter nose finger bomb damage corn expect marble",
             ),
             passphrase: String::new(),
         };
@@ -392,11 +415,8 @@ mod tests {
 
         let message = "Hello, FWM!".as_bytes();
         let signature = FreeWebMovementAddress::sign_message(&fwmaddress.private_key, message);
-        let is_valid = FreeWebMovementAddress::verify_message(
-            &fwmaddress.public_key,
-            message,
-            &signature
-        );
+        let is_valid =
+            FreeWebMovementAddress::verify_message(&fwmaddress.public_key, message, &signature);
         assert!(is_valid, "签名验证失败!");
     }
 
@@ -409,17 +429,27 @@ mod tests {
         println!("FreeWebMovementAddress JSON: {}", json);
 
         // 反序列化回对象
-        let fwmaddress2: FreeWebMovementAddress = serde_json
-            ::from_str(&json)
-            .expect("反序列化失败");
+        let fwmaddress2: FreeWebMovementAddress =
+            serde_json::from_str(&json).expect("反序列化失败");
 
         // 关键字段应一致
         assert_eq!(fwmaddress.to_string(), fwmaddress2.to_string());
         assert_eq!(fwmaddress.prefix, fwmaddress2.prefix);
         assert_eq!(fwmaddress.public_key, fwmaddress2.public_key);
         assert_eq!(fwmaddress.private_key, fwmaddress2.private_key);
-        assert_eq!(fwmaddress.mnemonic.to_string(), fwmaddress2.mnemonic.to_string());
+        assert_eq!(
+            fwmaddress.mnemonic.to_string(),
+            fwmaddress2.mnemonic.to_string()
+        );
         assert_eq!(fwmaddress.address, fwmaddress2.address);
+
+        let bytes = fwmaddress.public_key.to_bytes();
+        let public_key = FreeWebMovementAddress::to_public_key(&bytes);
+        assert_eq!(fwmaddress.public_key.to_string(), public_key.to_string());
+
+        let bytes = fwmaddress.private_key.to_bytes();
+        let private_key = FreeWebMovementAddress::to_private_key(&bytes);
+        assert_eq!(fwmaddress.private_key.to_string(), private_key.to_string());
     }
 
     #[test]
@@ -440,18 +470,25 @@ mod tests {
     #[test]
     fn test_basics() {
         println!("MAX_HUMAN_POPULATION: {}", MAX_HUMAN_POPULATION);
-        println!("AVERAGE_ASSETS_PER_ONE_IN_USD: {}", AVERAGE_ASSETS_PER_USER_AS_IN_USD);
+        println!(
+            "AVERAGE_ASSETS_PER_ONE_IN_USD: {}",
+            AVERAGE_ASSETS_PER_USER_AS_IN_USD
+        );
         println!("MAX_COIN_SUPPLY: {}", MAX_COIN_SUPPLY);
         println!("COIN_NAME: {}", COIN_NAME);
         println!("COIN_SYMBOL: {}", COIN_SYMBOL);
         println!("COIN_DECIMALS: {}", COIN_DECIMALS);
         assert_eq!(MAX_HUMAN_POPULATION, 10_000_000_000);
         assert_eq!(AVERAGE_ASSETS_PER_USER_AS_IN_USD, 1_000_000);
-        assert_eq!(MAX_COIN_SUPPLY, MAX_HUMAN_POPULATION * AVERAGE_ASSETS_PER_USER_AS_IN_USD * 100); // 10 trillion USD
-        assert_eq!(COIN_NAME, "Free Web Movement Coin - Zero Trust Zero Governance");
+        assert_eq!(
+            MAX_COIN_SUPPLY,
+            MAX_HUMAN_POPULATION * AVERAGE_ASSETS_PER_USER_AS_IN_USD * 100
+        ); // 10 trillion USD
+        assert_eq!(
+            COIN_NAME,
+            "Free Web Movement Coin - Zero Trust Zero Governance"
+        );
         assert_eq!(COIN_SYMBOL, "FWMC-ZZ");
         assert_eq!(COIN_DECIMALS, 8); // 1 ZZC = 0.00000001 USD
     }
 }
-
-
